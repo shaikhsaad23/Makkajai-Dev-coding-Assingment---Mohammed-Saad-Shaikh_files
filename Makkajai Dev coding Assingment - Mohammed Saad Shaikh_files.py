78312cda76a3c15dec3beee33a8ddf21b639044f
shaikhsaad23 <41883193+shaikhsaad23@users.noreply.github.com>
2021-03-15 14:43:31 +0530

    Add files via upload

---
+++ Makkajai Dev coding Assingment - Mohammed Saad Shaikh_files.py
@@ -0,0 +1,90 @@
+input1=list(map(str,input('Input Quantity, Item and price:').split()))
+
+
+quantity=int(input1[0])
+
+
+item=''.join(input1[1:-2])
+
+
+price=float(input1[-1])
+
+
+
+taxExempt={"book","Chochlate_Bar","Headache_pills"}
+
+
+
+basicTax={"music_CD","Bottle_of_perfume"}
+
+
+
+importedTax={"box_of_imported_Chochlate"}
+
+
+
+bothTax={"Imported_bottle_of_perfume"}
+
+
+
+if item in taxExempt:
+    
+    print("Output:")
+    
+    print(quantity, item, price)
+    
+
+
+elif item in basicTax:
+    
+    salesTax=quantity*10*price/100
+
+    taxprice=price+salesTax
+
+    print("Output:")
+
+    print(quantity, item, taxprice)
+
+    print("salesTax:",salesTax)
+
+    print("Total:" , taxprice+salesTax)
+
+    
+
+elif item in importedTax:
+
+    salesTax=quantity*5*(price/100)
+
+    taxprice=price+salesTax
+
+    print("Output:")
+
+    print("salesTax:",salesTax)
+
+    print("Total:" , taxprice+salesTax)
+
+
+
+elif item in bothTax:
+
+    salesTax=quantity*15*(price/100)
+
+    taxprice=price+salesTax
+
+    print("Output:")
+
+    print(quantity, item, taxprice)
+
+    print("salesTax:",salesTax)
+
+    print("Total:" , taxprice+salesTax)
+  
+
+
+
+
+  
+    
+    
+    
+    
